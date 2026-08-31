5afbb745311dc8e2cbce994a52aa9ef811837353
CaioLopes99 <caio.m.lopes1999@gmail.com>
2026-08-31 19:44:13 -0300

    ListaA2-Exercicio1.c

--- ListaA2-Exercicio1.c
+++ ListaA2-Exercicio1.c
@@ -5,17 +5,20 @@ int main() {
     int vetor[TAMANHO];
     int i;
 
+    // Leitura dos 10 números
     printf("Digite 10 numeros inteiros:\n");
     for (i = 0; i < TAMANHO; i++) {
         printf("Numero %d: ", i + 1);
         scanf("%d", &vetor[i]);
     }
 
+    // Exibe na ordem em que foram digitados
     printf("\nValores na ordem digitada:\n");
     for (i = 0; i < TAMANHO; i++) {
         printf("%d ", vetor[i]);
     }
 
+    // Exibe na ordem inversa
     printf("\n\nValores na ordem inversa:\n");
     for (i = TAMANHO - 1; i >= 0; i--) {
         printf("%d ", vetor[i]);
