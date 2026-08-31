d501125c608c973ea0fcabbb45f71027fafa0fa8
CaioLopes99 <caio.m.lopes1999@gmail.com>
2026-08-31 13:16:22 -0300

    Exercicio7.c

---
+++ Exercicio7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+   int main() {
+
+   printf("Tamanho dos tipos em bytes:\n");
+   printf("char........: %zu\n", sizeof(char));
+   printf("short.......: %zu\n", sizeof(short));
+   printf("int.........: %zu\n", sizeof(int));
+   printf("long........: %zu\n", sizeof(long));
+   printf("long long...: %zu\n", sizeof(long long));
+   printf("float.......: %zu\n", sizeof(float));
+   printf("double......: %zu\n", sizeof(double));
+   printf("long double.: %zu\n", sizeof(long double));
+
+  return 0;
+
+}
